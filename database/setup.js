1e9a8f74778fd4d94e8c79170c8a7a5623a12f27
muxmesh9 <meshmux@gmail.com>
2026-04-24 00:44:35 +0300

    Remove better-sqlite3 completely

--- database/setup.js
+++ database/setup.js
@@ -6,7 +6,7 @@
 
 'use strict';
 
-const Database = require('better-sqlite3');
+const Database = require('sqlite3').verbose();
 const bcrypt   = require('bcryptjs');
 const path     = require('path');
 const fs       = require('fs');
